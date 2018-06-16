fb634519f345d554413bd7d272635b30545f08e5
LAPTOP-87CAPFA5\godd7 <godd7776@gmail.com>
2018-06-17 06:10:47 +0900

    Add Vuforia And Make Test Case

---
+++ 8th_Eicon/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private MeshRenderer mr;
+
+    private void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mr.material.color = Random.ColorHSV();
+        }
+    }
+}
