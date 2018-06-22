5a0b4b0f4b9e19af3a99d252a04d1b0d20d9527f
LAPTOP-87CAPFA5\godd7 <godd7776@gmail.com>
2018-06-22 09:04:27 +0900

    Vuforia Test

---
+++ 8th_Eicon/Assets/CameraFocus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class CameraFocus : MonoBehaviour
+{
+    void Start()
+    {
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        VuforiaARController.Instance.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(
+                CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
